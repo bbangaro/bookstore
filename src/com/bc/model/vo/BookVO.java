3d8325885ada37b7a016eead1190a44ca697a92b
bbangaro <bbangaro@gmail.com>
2020-01-08 11:39:23 +0900

    2020-01-08

---
+++ src/com/bc/model/vo/BookVO.java
@@ -0,0 +1,9 @@
+package com.bc.model.vo;
+
+public class BookVO {
+	
+	int hName, lName, bPrice, salePrice, buyNum, bType ;
+	String hCode, lCode, bName, bCompany, status, st1, st2, 
+		   content, created, bCode, bImage, writerName, contents, eNum, eImage, aNum, audio;
+
+}
---
+++ src/com/bc/model/vo/GuestBookVO.java
@@ -0,0 +1,56 @@
+package com.bc.model.vo;
+
+public class GuestBookVO {
+	int reviewNum ;
+	String subject, rContent, upload ;
+	
+	public GuestBookVO() {}
+
+	public GuestBookVO(int reviewNum, String subject, String rContent, String upload) {
+		super();
+		this.reviewNum = reviewNum;
+		this.subject = subject;
+		this.rContent = rContent;
+		this.upload = upload;
+	}
+
+	public int getReviewNum() {
+		return reviewNum;
+	}
+
+	public void setReviewNum(int reviewNum) {
+		this.reviewNum = reviewNum;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getrContent() {
+		return rContent;
+	}
+
+	public void setrContent(String rContent) {
+		this.rContent = rContent;
+	}
+
+	public String getUpload() {
+		return upload;
+	}
+
+	public void setUpload(String upload) {
+		this.upload = upload;
+	}
+
+	@Override
+	public String toString() {
+		return "GuestBookVO [reviewNum=" + reviewNum + ", subject=" + subject + ", rContent=" + rContent + ", upload="
+				+ upload + "]";
+	}
+	
+
+}
