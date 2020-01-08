17d36d114deaf715588805fb3fd1cc272a243113
dnjswns9610 <58021684+dnjswns9610@users.noreply.github.com>
2020-01-08 19:18:52 +0900

    00

--- src/com/bc/notcontroller/NotListController.java
+++ src/com/bc/notcontroller/NotListController.java
@@ -7,12 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.bc.notcommand.Command;
+
 @WebServlet("/NotListController")
 public class NotListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Command comm = ;
 		
 	}
 
