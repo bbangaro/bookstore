d01f4e125523b311ec335b2369e628aeab7af914
DanHoBakMaCha <56007324+DanHoBakMaCha@users.noreply.github.com>
2020-01-08 16:36:45 +0900

    Update DAO.java

--- src/com/bc/model/dao/DAO.java
+++ src/com/bc/model/dao/DAO.java
@@ -10,11 +10,8 @@ public class DAO {
 		
 	//동적검색
 	public static void insetMember(MemberVO membervo) {
-		System.out.println("1");
 		SqlSession ss = DBService.getFactory().openSession(true);
-		System.out.println("2");
 		int count = ss.insert("memberinsert", membervo);
-		System.out.println("3");
 		System.out.println(count);
 		ss.close();
 		
