97a4b2e5da9fe60149c62af3531e360fc561edc4
javaara <lhh7944@gmail.com>
2020-01-08 12:43:36 +0900

    3

--- src/com/bc/model/vo/GuestBookVO.java
+++ src/com/bc/model/vo/GuestBookVO.java
@@ -1,17 +1,38 @@
 package com.bc.model.vo;
 
 public class GuestBookVO {
+<<<<<<< HEAD
 	private int reviewNum ;
 	private String subject, rContent, upload ;
+=======
+	int requestNum, reviewNum, qNum, nNum ;
+	String subject, rContent, upload, regdate, categor, wDate, writerName ;
+>>>>>>> 4f64a1580f75b97f5efd60e9774b122623cacff8
 	
 	public GuestBookVO() {}
 
-	public GuestBookVO(int reviewNum, String subject, String rContent, String upload) {
+	public GuestBookVO(int requestNum, int reviewNum, int qNum, int nNum, String subject, String rContent,
+			String upload, String regdate, String categor, String wDate, String writerName) {
 		super();
+		this.requestNum = requestNum;
 		this.reviewNum = reviewNum;
+		this.qNum = qNum;
+		this.nNum = nNum;
 		this.subject = subject;
 		this.rContent = rContent;
 		this.upload = upload;
+		this.regdate = regdate;
+		this.categor = categor;
+		this.wDate = wDate;
+		this.writerName = writerName;
+	}
+
+	public int getRequestNum() {
+		return requestNum;
+	}
+
+	public void setRequestNum(int requestNum) {
+		this.requestNum = requestNum;
 	}
 
 	public int getReviewNum() {
@@ -20,6 +41,22 @@ public class GuestBookVO {
 
 	public void setReviewNum(int reviewNum) {
 		this.reviewNum = reviewNum;
+	}
+
+	public int getqNum() {
+		return qNum;
+	}
+
+	public void setqNum(int qNum) {
+		this.qNum = qNum;
+	}
+
+	public int getnNum() {
+		return nNum;
+	}
+
+	public void setnNum(int nNum) {
+		this.nNum = nNum;
 	}
 
 	public String getSubject() {
@@ -46,11 +83,45 @@ public class GuestBookVO {
 		this.upload = upload;
 	}
 
+	public String getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+
+	public String getCategor() {
+		return categor;
+	}
+
+	public void setCategor(String categor) {
+		this.categor = categor;
+	}
+
+	public String getwDate() {
+		return wDate;
+	}
+
+	public void setwDate(String wDate) {
+		this.wDate = wDate;
+	}
+
+	public String getWriterName() {
+		return writerName;
+	}
+
+	public void setWriterName(String writerName) {
+		this.writerName = writerName;
+	}
+
 	@Override
 	public String toString() {
-		return "GuestBookVO [reviewNum=" + reviewNum + ", subject=" + subject + ", rContent=" + rContent + ", upload="
-				+ upload + "]";
+		return "GuestBookVO [requestNum=" + requestNum + ", reviewNum=" + reviewNum + ", qNum=" + qNum + ", nNum="
+				+ nNum + ", subject=" + subject + ", rContent=" + rContent + ", upload=" + upload + ", regdate="
+				+ regdate + ", categor=" + categor + ", wDate=" + wDate + ", writerName=" + writerName + "]";
 	}
 	
-
+	
+	
 }
