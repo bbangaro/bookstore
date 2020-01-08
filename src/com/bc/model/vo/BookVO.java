fbf270b199ac54ac47205c377bf90449feada579
bbangaro <bbangaro@gmail.com>
2020-01-08 12:02:54 +0900

    VO

--- src/com/bc/model/vo/BookVO.java
+++ src/com/bc/model/vo/BookVO.java
@@ -5,5 +5,230 @@ public class BookVO {
 	int hName, lName, bPrice, salePrice, buyNum, bType ;
 	String hCode, lCode, bName, bCompany, status, st1, st2, 
 		   content, created, bCode, bImage, writerName, contents, eNum, eImage, aNum, audio;
+	public BookVO() {}
+	
+	public BookVO(int hName, int lName, int bPrice, int salePrice, int buyNum, int bType, String hCode, String lCode,
+			String bName, String bCompany, String status, String st1, String st2, String content, String created,
+			String bCode, String bImage, String writerName, String contents, String eNum, String eImage, String aNum,
+			String audio) {
+		super();
+		this.hName = hName;
+		this.lName = lName;
+		this.bPrice = bPrice;
+		this.salePrice = salePrice;
+		this.buyNum = buyNum;
+		this.bType = bType;
+		this.hCode = hCode;
+		this.lCode = lCode;
+		this.bName = bName;
+		this.bCompany = bCompany;
+		this.status = status;
+		this.st1 = st1;
+		this.st2 = st2;
+		this.content = content;
+		this.created = created;
+		this.bCode = bCode;
+		this.bImage = bImage;
+		this.writerName = writerName;
+		this.contents = contents;
+		this.eNum = eNum;
+		this.eImage = eImage;
+		this.aNum = aNum;
+		this.audio = audio;
+	}
+
+	public int gethName() {
+		return hName;
+	}
+
+	public void sethName(int hName) {
+		this.hName = hName;
+	}
+
+	public int getlName() {
+		return lName;
+	}
+
+	public void setlName(int lName) {
+		this.lName = lName;
+	}
+
+	public int getbPrice() {
+		return bPrice;
+	}
+
+	public void setbPrice(int bPrice) {
+		this.bPrice = bPrice;
+	}
+
+	public int getSalePrice() {
+		return salePrice;
+	}
+
+	public void setSalePrice(int salePrice) {
+		this.salePrice = salePrice;
+	}
+
+	public int getBuyNum() {
+		return buyNum;
+	}
+
+	public void setBuyNum(int buyNum) {
+		this.buyNum = buyNum;
+	}
+
+	public int getbType() {
+		return bType;
+	}
+
+	public void setbType(int bType) {
+		this.bType = bType;
+	}
+
+	public String gethCode() {
+		return hCode;
+	}
+
+	public void sethCode(String hCode) {
+		this.hCode = hCode;
+	}
+
+	public String getlCode() {
+		return lCode;
+	}
+
+	public void setlCode(String lCode) {
+		this.lCode = lCode;
+	}
+
+	public String getbName() {
+		return bName;
+	}
+
+	public void setbName(String bName) {
+		this.bName = bName;
+	}
+
+	public String getbCompany() {
+		return bCompany;
+	}
+
+	public void setbCompany(String bCompany) {
+		this.bCompany = bCompany;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getSt1() {
+		return st1;
+	}
+
+	public void setSt1(String st1) {
+		this.st1 = st1;
+	}
+
+	public String getSt2() {
+		return st2;
+	}
+
+	public void setSt2(String st2) {
+		this.st2 = st2;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCreated() {
+		return created;
+	}
+
+	public void setCreated(String created) {
+		this.created = created;
+	}
+
+	public String getbCode() {
+		return bCode;
+	}
+
+	public void setbCode(String bCode) {
+		this.bCode = bCode;
+	}
+
+	public String getbImage() {
+		return bImage;
+	}
+
+	public void setbImage(String bImage) {
+		this.bImage = bImage;
+	}
+
+	public String getWriterName() {
+		return writerName;
+	}
+
+	public void setWriterName(String writerName) {
+		this.writerName = writerName;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String geteNum() {
+		return eNum;
+	}
+
+	public void seteNum(String eNum) {
+		this.eNum = eNum;
+	}
+
+	public String geteImage() {
+		return eImage;
+	}
+
+	public void seteImage(String eImage) {
+		this.eImage = eImage;
+	}
+
+	public String getaNum() {
+		return aNum;
+	}
+
+	public void setaNum(String aNum) {
+		this.aNum = aNum;
+	}
+
+	public String getAudio() {
+		return audio;
+	}
+
+	public void setAudio(String audio) {
+		this.audio = audio;
+	}
+
+	@Override
+	public String toString() {
+		return "BookVO [hName=" + hName + ", lName=" + lName + ", bPrice=" + bPrice + ", salePrice=" + salePrice
+				+ ", buyNum=" + buyNum + ", bType=" + bType + ", hCode=" + hCode + ", lCode=" + lCode + ", bName="
+				+ bName + ", bCompany=" + bCompany + ", status=" + status + ", st1=" + st1 + ", st2=" + st2
+				+ ", content=" + content + ", created=" + created + ", bCode=" + bCode + ", bImage=" + bImage
+				+ ", writerName=" + writerName + ", contents=" + contents + ", eNum=" + eNum + ", eImage=" + eImage
+				+ ", aNum=" + aNum + ", audio=" + audio + "]";
+	}
 
 }
--- src/com/bc/model/vo/GuestBookVO.java
+++ src/com/bc/model/vo/GuestBookVO.java
@@ -1,17 +1,33 @@
 package com.bc.model.vo;
 
 public class GuestBookVO {
-	int reviewNum ;
-	String subject, rContent, upload ;
+	int requestNum, reviewNum, qNum, nNum ;
+	String subject, rContent, upload, regdate, categor, wDate, writerName ;
 	
 	public GuestBookVO() {}
 
-	public GuestBookVO(int reviewNum, String subject, String rContent, String upload) {
+	public GuestBookVO(int requestNum, int reviewNum, int qNum, int nNum, String subject, String rContent,
+			String upload, String regdate, String categor, String wDate, String writerName) {
 		super();
+		this.requestNum = requestNum;
 		this.reviewNum = reviewNum;
+		this.qNum = qNum;
+		this.nNum = nNum;
 		this.subject = subject;
 		this.rContent = rContent;
 		this.upload = upload;
+		this.regdate = regdate;
+		this.categor = categor;
+		this.wDate = wDate;
+		this.writerName = writerName;
+	}
+
+	public int getRequestNum() {
+		return requestNum;
+	}
+
+	public void setRequestNum(int requestNum) {
+		this.requestNum = requestNum;
 	}
 
 	public int getReviewNum() {
@@ -20,6 +36,22 @@ public class GuestBookVO {
 
 	public void setReviewNum(int reviewNum) {
 		this.reviewNum = reviewNum;
+	}
+
+	public int getqNum() {
+		return qNum;
+	}
+
+	public void setqNum(int qNum) {
+		this.qNum = qNum;
+	}
+
+	public int getnNum() {
+		return nNum;
+	}
+
+	public void setnNum(int nNum) {
+		this.nNum = nNum;
 	}
 
 	public String getSubject() {
@@ -46,11 +78,45 @@ public class GuestBookVO {
 		this.upload = upload;
 	}
 
+	public String getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+
+	public String getCategor() {
+		return categor;
+	}
+
+	public void setCategor(String categor) {
+		this.categor = categor;
+	}
+
+	public String getwDate() {
+		return wDate;
+	}
+
+	public void setwDate(String wDate) {
+		this.wDate = wDate;
+	}
+
+	public String getWriterName() {
+		return writerName;
+	}
+
+	public void setWriterName(String writerName) {
+		this.writerName = writerName;
+	}
+
 	@Override
 	public String toString() {
-		return "GuestBookVO [reviewNum=" + reviewNum + ", subject=" + subject + ", rContent=" + rContent + ", upload="
-				+ upload + "]";
+		return "GuestBookVO [requestNum=" + requestNum + ", reviewNum=" + reviewNum + ", qNum=" + qNum + ", nNum="
+				+ nNum + ", subject=" + subject + ", rContent=" + rContent + ", upload=" + upload + ", regdate="
+				+ regdate + ", categor=" + categor + ", wDate=" + wDate + ", writerName=" + writerName + "]";
 	}
 	
-
+	
+	
 }
