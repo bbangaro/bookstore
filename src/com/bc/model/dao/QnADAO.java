e1213b849c4f2bb546bc6dff8572e1d0edf4dfb4
javaara <lhh7944@gmail.com>
2020-01-10 17:57:30 +0900

    hh

--- src/com/bc/model/dao/QnADAO.java
+++ src/com/bc/model/dao/QnADAO.java
@@ -1,24 +1,26 @@
 package com.bc.model.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
+import com.bc.model.vo.BookVO;
 import com.bc.model.vo.QnAVO;
 import com.bc.mybatis.DBService;
 
 public class QnADAO {
 	
-	public static List<QnAVO> getList() {
+	public static List<QnAVO> getList(Map<String, Integer> map) {
 		SqlSession ss = DBService.getFactory().openSession(true);
-		List<QnAVO> list = ss.selectList("selectall");
+		List<QnAVO> list = ss.selectList("selectall", map);
 		ss.close();
 		return list;
 	}
 	
-	public static int insertQnA(int qNum) {
+	public static int insertQnA(Map<String, String> map) {
 		SqlSession ss = DBService.getFactory().openSession(true);
-		int result = ss.insert("insert", qNum);
+		int result = ss.insert("insert", map);
 		ss.close();
 		return result;
 	}
@@ -28,5 +30,19 @@ public class QnADAO {
 		QnAVO vo = ss.selectOne("selectone", qNum);
 		ss.close();
 		return vo;
+	}
+	
+	public static int deleteQnA(int qNum) {
+		SqlSession ss = DBService.getFactory().openSession(true);
+		int result = ss.delete("delete", qNum);
+		ss.close();
+		return result;
+	}
+	
+	public static int getTotalCount() {
+		SqlSession ss = DBService.getFactory().openSession(true);
+		int result = ss.selectOne("total");
+		ss.close();
+		return result;
 	}
 }
