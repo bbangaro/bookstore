3a9a9506bb9a2ad068a9ec5d4d3d29adb69ec197
DanHoBakMaCha <56007324+DanHoBakMaCha@users.noreply.github.com>
2020-01-16 19:41:05 +0900

    sin

--- src/com/bc/detailPage/detailPage.java
+++ src/com/bc/detailPage/detailPage.java
@@ -2,7 +2,9 @@ package com.bc.detailPage;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -32,19 +34,36 @@ public class detailPage extends HttpServlet {
 		if (action == null || action.contentEquals("/Page")) {
 			
 			String bCode = request.getParameter("bCode");
+			String pageNum = request.getParameter("pageNum");
+			System.out.println(pageNum);
 			System.out.println(bCode);
 			
+			if(pageNum == null) {
+				pageNum = "1";
+			}
+			
 			BookVO bookvo = TakDAO.detailPage(bCode);
-			List<ReviewVO> reviewvo = TakDAO.detailPageReview(bCode);
+			Map<String, Integer> detailPageReview_ = new HashMap<>();
 			
 			
+			detailPageReview_.put("bCode", Integer.parseInt(bCode));
+			detailPageReview_.put("pageNum", Integer.parseInt(pageNum));
+			
+			List<ReviewVO> reviewvo = TakDAO.detailPageReview(detailPageReview_);
+			
+			request.setAttribute("pageNum", pageNum);
 			request.setAttribute("bookvo", bookvo);
 			request.setAttribute("reviewvo", reviewvo);
 			nextPage = "/pages/detailPage.jsp";
 
 		} else if (action.contentEquals("/AddForm")) {
 			nextPage = "/pages/detailPageAddForm.jsp";
-		} else if (action.contentEquals("/insertForm")) {
+			
+		}else if(action.contentEquals("/session")) {
+			HttpSession session = request.getSession();
+			session.setAttribute("reviewNum", "0");
+			nextPage = "/pages/detailPageAddForm.jsp";
+		}else if (action.contentEquals("/insertForm")) {
 			ServletContext sc = this.getServletContext();
 			String path = sc.getRealPath("/upload");
 			System.out.println(path);
@@ -56,7 +75,7 @@ public class detailPage extends HttpServlet {
 			String subject = mr.getParameter("subject");
 			String rContent = mr.getParameter("rContent");
 			String myFile = mr.getFilesystemName("myFile");
-			String parentno = (String) session.getAttribute("reviewNum");
+			String parentno = mr.getParameter("reviewNum");
 			String id = (String) session.getAttribute("id");
 			String bCode = (String) session.getAttribute("b_Code");
 
@@ -66,7 +85,9 @@ public class detailPage extends HttpServlet {
 			System.out.println(rContent);
 			System.out.println(subject);
 			System.out.println(myFile);
-
+			
+			
+			System.out.println("ï¿½ï¿½ï¿½ï¿½;;");
 			ReviewVO reviewvo = new ReviewVO();
 			reviewvo.setParentno(parentno);
 			reviewvo.setUpload(myFile);
@@ -74,11 +95,13 @@ public class detailPage extends HttpServlet {
 			reviewvo.setMemberId(id);
 			reviewvo.setSubject(subject);
 			reviewvo.setrContent(rContent);
+			System.out.println(reviewvo.getUpload());
+			System.out.println(reviewvo.getParentno());
 
 			int i = TakDAO.inersertReviewvo(reviewvo);
 
 			PrintWriter pw = response.getWriter();
-			pw.print("<script>alert('±Û¾²±â ¿Ï·á');" + "location.href='/bookstore/detail/Page?bCode="
+			pw.print("<script>alert('ï¿½Û¾ï¿½ï¿½ï¿½ ï¿½Ï·ï¿½');" + "location.href='/bookstore/detail/Page?bCode="
 					+ Integer.parseInt(bCode) + "'</script>");
 
 		} else if (action.contentEquals("/CheckForm")) {
@@ -127,7 +150,7 @@ public class detailPage extends HttpServlet {
 			reviewvo.setrContent(rContent);
 			int i = TakDAO.updateReviewvo(reviewvo);
 			PrintWriter pw = response.getWriter();
-			pw.print("<script>alert('¼öÁ¤ ¿Ï·á');" + "location.href='/bookstore/detail/Page?bCode="
+			pw.print("<script>alert('ï¿½ï¿½ï¿½ï¿½ ï¿½Ï·ï¿½');" + "location.href='/bookstore/detail/Page?bCode="
 					+ Integer.parseInt(bCode) + "'</script>");
 		} else if (action.contentEquals("/deleteForm")) {
 			String reviewNum = request.getParameter("reviewNum");
@@ -136,7 +159,7 @@ public class detailPage extends HttpServlet {
 			System.out.println("bCodr: " + bCode);
 			TakDAO.deleteReviewvo(reviewNum);
 			PrintWriter pw = response.getWriter();
-			pw.print("<script>alert('»èÁ¦ ¿Ï·á');" + "location.href='/bookstore/detail/Page?bCode="
+			pw.print("<script>alert('ï¿½ï¿½ï¿½ï¿½ ï¿½Ï·ï¿½');" + "location.href='/bookstore/detail/Page?bCode="
 					+ Integer.parseInt(bCode) + "'</script>");
 		}
 		RequestDispatcher dispatcher = request.getRequestDispatcher(nextPage);
