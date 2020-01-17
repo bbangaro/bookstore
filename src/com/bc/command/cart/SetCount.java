2ecb21a704f9905c86c84af2b84695a55c9cebd7
javaara <lhh7944@gmail.com>
2020-01-17 14:40:43 +0900

    장바구니 파라미터 연결(bType제외)

--- src/com/bc/command/cart/SetCount.java
+++ src/com/bc/command/cart/SetCount.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.bc.model.dao.CartDAO;
 import com.bc.model.vo.CartVO;
@@ -15,31 +16,35 @@ public class SetCount implements Command {
 
 	@Override
 	public String exec(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession(true);
+		
+		String memberId = (String)session.getAttribute("id");
+		
+		String bCode = request.getParameter("bCode");
+		int bType = Integer.parseInt(request.getParameter("bType"));
 
-		// String bCode = request.getParameter("bCode");
-		// int bType = Integer.parseInt(request.getParameter("bType"));
-		String bCode = "19"; // 코드랑 타입 넘겨받기!!!!!!!!!!!
-		int bType = 1;
+		
 		CartVO vo = new CartVO();
 		vo.setbCode(bCode);
 		vo.setbType(bType);
-		vo.setMemberId("test1");
-		int quantity = 3; //수량 넘겨받기~~~~~~~
+		vo.setMemberId(memberId);
+		int quantity = Integer.parseInt(request.getParameter("quantity"));
+		System.out.println("quantity:" + quantity);
 		
 		int count = CartDAO.findProduct(vo);
 		
 		Map<String, Object> map = new HashMap<>();
 		map.put("bType", bType);
 		map.put("bCode", bCode);
-		map.put("quantity", 3); //개수 수정!!!!!!!!!!!!!!!
-		map.put("memberId", "test1");
+		map.put("quantity", quantity);
+		map.put("memberId", memberId);
 		
 		if (count > 0) {
-			//총 합계 금액 변경!!!!!!!!!
 			CartDAO.setCount(map);
 		}
 		
-		return "cart.jsp";
+		return "CartListController";
 	}
 
 }
